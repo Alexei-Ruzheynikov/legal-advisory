b47828c9066a9fab5cce86ea995978f534f07e13
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-10-21 14:32:21 +0300

    Создал тему в underscores.me; style.css сократил до 15 строк и отредактирвоал;Активировал тему в админке;Создал архивные и постовые файлы соответственно, где был макет;Создал шаблон страницы контакты template-contact.php и страницу в админке связал, так же и для "О компании" template-about.php и главной - template-home.php и в админке в настрйоках - чтение - выбрал "главную" -статической страницей;Создал папку assets и закинулв нее все папки верстки; Создал header.php и footer.php; В functions.php сделал подключение стилей и скриптов.

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * legal-advisory Theme Customizer
+ *
+ * @package legal-advisory
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function legal_advisory_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'legal_advisory_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'legal_advisory_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'legal_advisory_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function legal_advisory_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function legal_advisory_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function legal_advisory_customize_preview_js() {
+	wp_enqueue_script( 'legal-advisory-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'legal_advisory_customize_preview_js' );
